239a042708da79d37eaad29b3de89739698aebb8
SUSHMITHA SHETTY <ssushmitha904@gmail.com>
2021-10-11 13:05:14 +0530

    First Eclipse Commit

---
+++ src/TestDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author SUSHMITHA SHETTY
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+	}
+
+}
